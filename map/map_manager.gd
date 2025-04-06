8be0916d1732a1886b6a53b96e468048c68f7c5e
Emery Smith <smith.emerym@gmail.com>
2025-04-06 10:41:08 -0400

    pathfinding
    in progress

--- map/map_manager.gd
+++ map/map_manager.gd
@@ -11,6 +11,27 @@ class_name MapManager
 @onready var left_wall_layer: TileMapLayer = $LeftWallLayer
 @onready var high_left_wall_layer: TileMapLayer = $HighLeftWallLayer
 @onready var high_left_wall_layer_2: TileMapLayer = $HighLeftWallLayer2
+var astar := AStarGrid2D.new()
 
 #TODO spawn shadows
 #TODO spawn chunks
+
+func _ready() -> void:
+	var start_rect = ground_layer.get_used_rect()
+	var tile_size = ground_layer.get_tileset().tile_size
+	astar.region = start_rect
+	astar.cell_size = tile_size
+	astar.default_compute_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	astar.default_estimate_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	astar.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	astar.update()
+	
+	for child in get_children():
+		if child is TileMapLayer:
+
+			for i in start_rect.x:
+				for j in start_rect.y:
+					var tile_coords = Vector2i(i, j)
+					var tile_data = child.get_cell_data(tile_coords)
+					if tile_data and tile_data.get_custom_data("wall") == true:
+						astar.set_point_solid(tile_coords)
